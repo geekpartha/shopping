3609b7cac56978816ee078141afff779058f83ad
geekpartha <partha_sarathi@hotmail.com>
2019-03-26 15:50:10 +0530

    commit initialized

---
+++ src/data.js
@@ -0,0 +1,103 @@
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Alphabet - A",
+    img: "img/product-1.png",
+    price: 25,
+    company: "A-Company",
+    info:"A is the first letter and the first vowel of the modern English alphabet and the ISO basic Latin alphabet. It is similar to the Ancient Greek letter alpha, from which it derives. The lowercase version can be written in two forms: the double-storey a and single-storey ɑ.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Alphabet - B",
+    img: "img/product-2.png",
+    price: 16,
+    company: "B-Company",
+    info:"B is the second letter of the ISO basic Latin alphabet. It represents the voiced bilabial stop in many languages, including English. In some other languages, it is used to represent other bilabial consonants. ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Alphabet - C",
+    img: "img/product-3.png",
+    price: 8,
+    company: "C-Company",
+    info:"C is the third letter in the English alphabet and a letter of the alphabets of many other writing systems which inherited it from the Latin alphabet. It is also the third letter of the ISO basic Latin alphabet. It is named cee (pronounced /siː/) in English.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Alphabet - D",
+    img: "img/product-4.png",
+    price: 12,
+    company: "D-Company",
+    info:"D (named dee /diː/) is the fourth letter of the modern English alphabet and the ISO basic Latin alphabet. The equivalent Greek letter is Delta, Δ. The Roman numeral Ⅾ represents the number 500.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Alphabet - E",
+    img: "img/product-5.png",
+    price: 9,
+    company: "E-Company",
+    info:"E (named e /iː/, plural ees) is the fifth letter and the second vowel in the modern English alphabet and the ISO basic Latin alphabet. It is the most commonly used letter in many languages, including Czech, Danish, Dutch, English, French, German, Hungarian, Latin, Latvian, Norwegian, Spanish, and Swedish",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Alphabet - F",
+    img: "img/product-6.png",
+    price: 17,
+    company: "F-Company",
+    info:"F (named ef /ɛf/) is the sixth letter in the modern English alphabet and the ISO basic Latin alphabet. In the English writing system ⟨f⟩ is used to represent the sound /f/, the voiceless labiodental fricative. It is often doubled at the end of words. Exceptionally, it represents the voiced labiodental fricative /v/ in the common word 'of'.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Alphabet - G",
+    img: "img/product-7.png",
+    price: 27,
+    company: "G-Company",
+    info:"G (named gee /dʒiː/) is the 7th letter in the ISO basic Latin alphabet.  The letter 'G' was introduced in the Old Latin period as a variant of 'C' to distinguish voiced /ɡ/ from voiceless /k/. The recorded originator of 'G' is freedman Spurius Carvilius Ruga, the first Roman to open a fee-paying school, who taught around 230 BCE.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Alphabet - H",
+    img: "img/product-8.png",
+    price: 2,
+    company: "H-Company",
+    info:"H (named aitch /eɪtʃ/ or, regionally, haitch /heɪtʃ/, plural aitches) is the eighth letter in the ISO basic Latin alphabet.  The haitch pronunciation of h has spread in England, being used by approximately 24% of English people born since 1982, and polls continue to show this pronunciation becoming more common among younger native speakers.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+    id: 1,
+    title: "Alphabet - A",
+    img: "img/product-1.png",
+    price: 10,
+    company: "A-Company",
+    info:"A is the first letter and the first vowel of the modern English alphabet and the ISO basic Latin alphabet. It is similar to the Ancient Greek letter alpha, from which it derives. The lowercase version can be written in two forms: the double-storey a and single-storey ɑ. The latter is commonly used in handwriting and fonts based on it, especially fonts intended to be read by children.",
+    inCart: false,
+    count: 0,
+    total: 0
+};
